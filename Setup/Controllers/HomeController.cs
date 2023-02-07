8f226b9350695641456b81c06461483a9ea2b7b5
jessica <86186397+jessMechanic@users.noreply.github.com>
2023-02-07 20:10:04 +0100

    added the cookie view count

---
+++ Setup/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Setup.Models;
+using System.Diagnostics;
+
+namespace Setup.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private const string PageViews = "PageViews";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+           UpdatePageViewCookie();
+            return View( new DataContext(){views = GetPageViewCookie()} );
+        }
+
+        public IActionResult Privacy()
+        {
+            UpdatePageViewCookie();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public void UpdatePageViewCookie()
+        {
+            var currentCookieValue = Request.Cookies[PageViews];
+
+            if (currentCookieValue == null)
+            {
+                Response.Cookies.Append(PageViews, "1");
+            }
+            else
+            {
+                var newCookieValue = short.Parse(currentCookieValue) + 1;
+
+                Response.Cookies.Append(PageViews, newCookieValue.ToString());
+            }
+            
+            
+        }
+
+        public int GetPageViewCookie()
+        {
+            var currentCookieValue = Request.Cookies[PageViews];
+            return int.Parse(currentCookieValue);
+        }
+    }
+}
---
+++ Setup/Views/Home/UserProfile.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Setup.Views.Home;
+
+public class UserProfile : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
