e879733f609e0aafd2c1bb1e9f5138a836e989cc
jessica <86186397+jessMechanic@users.noreply.github.com>
2023-02-09 21:22:22 +0100

    cleaned op reposetory

--- Setup/Controllers/DataContext.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Setup.Controllers;
-using Microsoft.AspNetCore.Mvc;
-public  class DataContext : Controller
-{
-   private const string PageViews = "PageViews";
-   public int views { get; set; }
-}
-      
