b512269fe7a86d5ba876d9ed71739ada1bbf5395
jessica <86186397+jessMechanic@users.noreply.github.com>
2023-02-10 12:59:36 +0100

    Merged with VMC ASP.net

---
+++ Setup/Controllers/DataContext.cs
@@ -0,0 +1,8 @@
+﻿namespace Setup.Controllers;
+using Microsoft.AspNetCore.Mvc;
+public  class DataContext : Controller
+{
+   private const string PageViews = "PageViews";
+   public int views { get; set; }
+}
+      
---
+++ Setup/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Setup.Models;
+using System.Diagnostics;
+
+namespace Setup.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private const string PageViews = "PageViews";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+           UpdatePageViewCookie();
+            return View( new DataContext(){views = GetPageViewCookie()} );
+        }
+
+        public IActionResult Privacy()
+        {
+            UpdatePageViewCookie();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public void UpdatePageViewCookie()
+        {
+            int currentCookieValue = GetPageViewCookie();
+
+            currentCookieValue++;
+            Response.Cookies.Append(PageViews, currentCookieValue.ToString());
+        }
+
+        public int GetPageViewCookie()
+        {
+            string? currentCookieValue = Request.Cookies[PageViews];
+
+            if(currentCookieValue == null) return 0;
+
+            return int.Parse(currentCookieValue);
+        }
+    }
+}
